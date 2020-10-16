f635f60f4f186b7cab7e56ba068b286666383f71
Vasiliy N. Troyan <igelinmist@gmail.com>
2020-10-16 18:17:16 +0600

    Ommitted environment variables

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import { Environment } from './interface';
-
-export const environment: Environment = {
-  production: true,
-  apiKey: 'AIzaSyBG7IRhQxR3bqpmA2nwFsm9ncoJMaTX2cE',
-  fbDbUrl: 'https://angular-blog-36102.firebaseio.com',
-};
